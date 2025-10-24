a4aef0e9f128f76d5282dbc6a5be2e50baf58699
Dylan Cawsey <dylancawsey@gmail.com>
2025-10-24 18:25:24 +1100

    Add status logging for script

--- scripts/export-current-standings.sh
+++ scripts/export-current-standings.sh
@@ -1,12 +1,23 @@
-# Get all env variables from .env file
-set -a; source .env; set +a
+#!/bin/sh
 
+echo
+echo "**** START: `date`"
+
+echo "Loading environment variables from .env file..."
+set -a; source .env; set +a
+echo "Environment variables load status: $?"
+
+echo "Activating virtual environment and scraping NBA standings..."
 source .venv/bin/activate
 python .
+echo "Scraping status: $?"
 deactivate
 
 SEASON_START_YEAR=$((SEASON_END_YEAR - 1))
 
 SEASON_PERIOD="$SEASON_START_YEAR-$SEASON_END_YEAR"
 FILENAME='standings'
+
+echo "Exporting current standings to nba-skins-draft-results repo..."
 cp -nf ".data/${FILENAME}_${SEASON_PERIOD}.json" "../nba-skins-draft-results/src/data/$SEASON_PERIOD/$FILENAME.json"
+echo "Copy status: $?"
